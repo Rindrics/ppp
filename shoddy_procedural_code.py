19a47c48e698003297231443b81f1d392b46e78f
Akira <smxshxishxad@me.com>
2020-08-29 16:18:00 +0900

    :bug: Correct list number

--- shoddy_procedural_code.py
+++ shoddy_procedural_code.py
@@ -10,7 +10,7 @@ def get_human_choice():
     return OPTIONS[choice_number - 1]
 
 def print_options():
-    print('\n'.join(f'({i}) {option.title()}' for i,
+    print('\n'.join(f'({i + 1}) {option.title()}' for i,
                     option in enumerate(OPTIONS)))
 
 def print_choices(human_choice, computer_choice):
