4c9563bcebf81b2b63787f2dd864bb45b7fc77a6
Akira <smxshxishxad@me.com>
2020-08-30 17:10:25 +0900

    :recycle: Refactor with get_matches_for_pattern()

--- review_analize.py
+++ review_analize.py
@@ -1,16 +1,22 @@
 import re
 
-product_review = '''This is a fine milk, but the product
-line appears to be limited in available colors. I
-could only find white.'''
+def get_matches_for_pattern(pattern, string):
+    matches = pattern.findall(string)
+    return [match[0] for match in matches]
+
+product_review = '...'
 
 sentence_pattern = re.compile(r'(.*?\.)(\s|$)', re.DOTALL)
-matches = sentence_pattern.findall(product_review)
-sentences = [match[0] for match in matches]
+sentences = get_matches_for_pattern(
+    sentence_pattern,
+    product_review
+)
 
 word_pattern = re.compile(r"([\w\-']+)([\s,.])?")
-for sentence in sentences:
-    matches = word_pattern.findall(sentence)
-    words = [match[0] for match in matches]
-    print(words)
 
+for sentence in sentences:
+    words = get_matches_for_pattern(
+        word_pattern,
+        sentence
+    )
+    print(words)
