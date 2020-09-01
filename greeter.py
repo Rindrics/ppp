c8d82da501ff1de6f34f3807b1be457d47de85da
Akira Hayashi <smxshxishxad@me.com>
2020-09-01 10:27:30 +0900

    :up: Update

--- greeter.py
+++ greeter.py
@@ -5,12 +5,14 @@ def _part_of_day(self):
     time = some_time()
     if time < 12:
         return "morning"
-    elif: time >= 12 | time < 17
+    elif time >= 12 | time < 17:
         return "afternoon"
     else:
         return "evening"
     
 def greet(self, store):
-     print(f"Hi, welcome to {store}!")
-     print(f"How’s your {day} {part of day} going?")
-     print(f"Here’s a coupon for 20% off!")
+    day         = self._day()
+    part_of_day = self._part_of_day()
+    print(f"Hi, welcome to {store}!")
+    print(f"How’s your {day} {part_of_day} going?")
+    print(f"Here’s a coupon for 20% off!")
